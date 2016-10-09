ac2241573eeeebb36fbcea93428618d7d058eb2b
Luis Fernandes <luisfmfernandes@gmail.com>
2016-10-09 18:22:12 +0100

    Fix installer.

--- reaction_ecommerce/utils/install.py
+++ reaction_ecommerce/utils/install.py
@@ -101,7 +101,7 @@ def after_install():
 
 	from reaction_ecommerce.commands.site import update_password
 	site = frappe.local.site
-	update_password([site], "")
+	#update_password([site], "")
 	info = """
 		**********************************************************
 		*   Every time you need to change admin password you     *
