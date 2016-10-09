516f3248ccc2b798b26cdb1519323b0b03b227ba
Luis Fernandes <luisfmfernandes@gmail.com>
2016-10-09 18:58:01 +0100

    Add hash admin password to site_config.json.

--- reaction_ecommerce/commands/site.py
+++ reaction_ecommerce/commands/site.py
@@ -8,7 +8,7 @@ from frappe.commands import pass_context
 
 
 def update_password(sites, user_password):
-	import getpass
+	import getpass, os
 	from frappe.utils.password import update_password as upd_pwd
 
 	for site in sites:
@@ -21,8 +21,15 @@ def update_password(sites, user_password):
 			# set password has meteor.js send to the server; as SHA256.
 			from reaction_ecommerce.utils.users import hashpw
 			frappe.connect()
-			upd_pwd('Administrator', hashpw(user_password))
+			pwd_hash = hashpw(user_password)
+			upd_pwd('Administrator', pwd_hash)
 			frappe.db.commit()
+			site_path = frappe.get_site_path()
+			site_config = frappe.get_file_json(os.path.join(site_path, "site_config.json"))
+			site_config["admin_password"] = pwd_hash
+			#write to file.
+			with open(os.path.join(site_path, "site_config.json"), 'w') as txtfile:
+				txtfile.write(frappe.as_json(site_config))
 			user_password = None
 		finally:
 			frappe.destroy()
